ae15f41895644d780e4383ca8e8fb65d00e9bf9a
zarasah <zarinasah1989@gmail.com>
2025-10-24 12:51:19 +0400

    setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as process from "process";
+import {ConfigModule} from "@nestjs/config";
+import {dbConnectionsConfig} from "./configs/ormconfig";
+import {DatabaseModule} from "./modules/database/database.module";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      expandVariables: true,
+      load: [dbConnectionsConfig],
+    }),
+
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
